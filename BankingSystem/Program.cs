8979e06f128a59babddde4f0f6d0c502de7c072b
QasimMehmood <QasimMehmood@DESKTOP-036EBRK>
2024-06-07 06:37:20 -0700

    Add project files.

---
+++ BankingSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BakingSystem
+{
+    class Program {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Banking System");
+        }
+    
+    }
+}
